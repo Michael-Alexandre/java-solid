5b6a711a8d14518f0bc03e4305e52536b7b2f9d6
Michael <michael.alexandre27@gmail.com>
2024-02-04 15:59:34 -0300

    update srp.java

--- srp.java
+++ srp.java
@@ -1,3 +1,6 @@
+/**
+ * SOLID - Single Responsibility Principle
+ */
 import java.util.ArrayList;
 import java.util.List;
 
@@ -6,8 +9,8 @@ class Journal
   private final List<String> entries = new ArrayList<>();
   private static int index =0;
 
-  public void inputEntry(String texString) {
-    entries.add("" + (++index) + texString);
+  public void inputEntry(String textString) {
+    entries.add("" + (++index) + textString);
   }
 
   public void deleteEntry(int index){
@@ -15,5 +18,17 @@ class Journal
   }
 
   @Override
+  public String toString(){
+    return String.join(System.lineSeparator(), entries);
+  }
+}
+
+class Demonstration {
+
+    public static void main(String[] argStrings) {
+      Journal j = new Journal();
+      j.inputEntry("I ate a bug");
+      j.inputEntry("It was a delicious bug");
+    }
   
 }
