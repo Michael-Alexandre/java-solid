433db19699cf94586c7fc93d7c844bfc3f774405
Michael <michael.alexandre27@gmail.com>
2024-02-15 21:05:47 -0300

    Solid

---
+++ dip/dip.java
@@ -0,0 +1,7 @@
+/*dependency inversion principle
+A. high-level modules should not depend on low-level modules.
+Both should depend on abstractions.
+
+B. abstractions should not depend on details.
+Details should depend on abstractions.
+*/
